3446bba810b717375f857bf230eee0b9d187e58e
Michael <m.oconnell56@yahoo.ie>
2022-01-02 22:39:38 +0000

    wip

--- src/lib/components/a-week-alone/Artwork.svelte
+++
@@ -1,9 +0,0 @@
-<div class="card bg-base-200 lg:card-side rounded-xl mb-2">
-  <img class="h-full w-full md:w-1/2" src="/aweekalonecover.jpg">
-  <div class="card-body">
-    <h2 class="card-title">About the artwork</h2> 
-    <p class="mb-5">
-      A story about the album art. This release and my previous release called “whatever” both host a baby photo of me as the art. This photo is of an old vhs tape my Mam has lying around. It’s myself, as a baby, attempting to smoke one of my sister Catherines fags. I believe they were John Player Silk Cut Purples. “They were sopping wet”, my sister recalls. Additionally, although not visible, I have a bic lighter in my hand which I’m intently using to try and spark up.
-    </p>
-  </div>
-</div> 
